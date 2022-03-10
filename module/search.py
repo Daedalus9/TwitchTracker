1da21ad5bce8c6dce7acdfbf8f33ffd84eb464a1
Daedalus9 <longo.luca94@hotmail.it>
2022-03-10 14:48:32 +0100

    refactor: added search function

--- module/search.py
+++ module/search.py
@@ -1,16 +1,17 @@
 import requests
 from datetime import datetime
 
-us = open("list.txt", "r")
-print("Inizio analisi il " + str(datetime.now()))
-for line in us:
-    url = "https://tmi.twitch.tv/group/user/"+line.lower()[:-1]+"/chatters"
-    r=requests.get(url).json()
-    chat=r.get('chatters')['viewers']
-    st = open("list.txt", "r")
-    for user in st:
-        if(user[:-1] in chat):
-            print(user[:-1] + " sta guardando " + line[:-1])
-    st.close()
-us.close()
-print("Fine analisi il " + str(datetime.now()))
+def search():
+    us = open("list.txt", "r")
+    print("Inizio analisi il " + str(datetime.now()))
+    for line in us:
+        url = "https://tmi.twitch.tv/group/user/"+line.lower()[:-1]+"/chatters"
+        r=requests.get(url).json()
+        chat=r.get('chatters')['viewers']
+        st = open("list.txt", "r")
+        for user in st:
+            if(user[:-1] in chat):
+                print(user[:-1] + " sta guardando " + line[:-1])
+        st.close()
+    us.close()
+    print("Fine analisi il " + str(datetime.now()))
