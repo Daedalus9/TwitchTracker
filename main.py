905be2de472ad09c5dd5e0cd6b49cca214c06655
Daedalus9 <longo.luca94@hotmail.it>
2022-03-15 11:07:12 +0100

    feat: added store function

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import sys
 from module.search import search
+from module.store import store
 
 def main():
-    search()
+    args = sys.argv[1:]
+    if len(args)>0:
+        if args[0] == '--search':
+            search()
+        elif args[0] == '--store':
+            store()
 
 if __name__ == '__main__':
     main()
